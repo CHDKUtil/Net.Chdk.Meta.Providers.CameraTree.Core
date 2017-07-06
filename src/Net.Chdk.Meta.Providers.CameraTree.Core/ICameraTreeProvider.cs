76191f820c3fae8c448d30cfb89c2bbc39ed8fb1
Dmitry Shechtman <damnian@gmail.com>
2017-07-07 01:13:58 +0300

    Add solution

---
+++ src/Net.Chdk.Meta.Providers.CameraTree.Core/ICameraTreeProvider.cs
@@ -0,0 +1,11 @@
+﻿using Net.Chdk.Meta.Model.CameraTree;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Net.Chdk.Meta.Providers.CameraTree
+{
+    public interface ICameraTreeProvider
+    {
+        IDictionary<string, TreePlatformData> GetCameraTree(Stream stream);
+    }
+}
