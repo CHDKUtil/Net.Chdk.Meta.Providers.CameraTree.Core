d1b1b02702cb801d326d00031acffa852f71e1d1
Dmitry Shechtman <damnian@gmail.com>
2017-07-07 20:43:34 +0300

    Replace stream with path

--- src/Net.Chdk.Meta.Providers.CameraTree.Core/ICameraTreeProvider.cs
+++ src/Net.Chdk.Meta.Providers.CameraTree.Core/ICameraTreeProvider.cs
@@ -1,11 +1,10 @@
 ﻿using Net.Chdk.Meta.Model.CameraTree;
 using System.Collections.Generic;
-using System.IO;
 
 namespace Net.Chdk.Meta.Providers.CameraTree
 {
     public interface ICameraTreeProvider
     {
-        IDictionary<string, TreePlatformData> GetCameraTree(Stream stream);
+        IDictionary<string, TreePlatformData> GetCameraTree(string path);
     }
 }
