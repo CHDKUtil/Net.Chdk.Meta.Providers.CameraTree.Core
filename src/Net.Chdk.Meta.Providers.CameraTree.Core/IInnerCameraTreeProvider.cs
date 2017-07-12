c37b6c2756244fcfa0f4d93aa7b1de9bd193c773
Dmitry Shechtman <damnian@gmail.com>
2017-07-12 19:53:29 +0300

    Add IInnerCameraTreeProvider

---
+++ src/Net.Chdk.Meta.Providers.CameraTree.Core/IInnerCameraTreeProvider.cs
@@ -0,0 +1,11 @@
+﻿using Net.Chdk.Meta.Model.CameraTree;
+using System.Collections.Generic;
+
+namespace Net.Chdk.Meta.Providers.CameraTree
+{
+    public interface IInnerCameraTreeProvider
+    {
+        IDictionary<string, TreePlatformData> GetCameraTree(string path);
+        string Extension { get; }
+    }
+}
